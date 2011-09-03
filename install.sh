049379d93ec7646c1f04722e2494adbb9a947854
Philippe Pepiot <phil@philpep.org>
2011-09-03 13:57:09 +0200

    Add install.sh, install script

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -e
+
+FILES="gitconfig inputrc screenrc tmux.conf vim vimrc zshrc"
+BASEPATH=$(pwd)
+
+if [ -z "${HOME}" ]
+then
+	echo "[!] need \$HOME set"
+	exit 1
+fi
+
+for file in $FILES
+do
+	src="${BASEPATH}/${file}"
+	dst="${HOME}/.${file}"
+
+	if [ -e "${dst}" ]
+	then
+		echo "[!] ${dst} already exist, skip"
+	else
+		echo ln -s '"'${src}'"' '"'${dst}'" ? [y/n] '
+		read ans
+		if [ "${ans}" = "y" ]
+		then
+			ln -s "${src}" "${dst}"
+		fi
+	fi
+done
+
+echo ln -s ${BASEPATH}/wmfs ${HOME}/.config/wmfs '? [y/n] '
+read ans
+if [ "${ans}" = "y" ]
+then
+	mkdir -p ${HOME}/.config
+	ln -s ${BASEPATH}/wmfs ${HOME}/.config/wmfs
+fi

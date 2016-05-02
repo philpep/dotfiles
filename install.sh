a7598462123d501bd0f99bcc5d55e452802f36d5
Philippe Pepiot <philippe.pepiot@logilab.fr>
2016-05-02 18:03:37 +0200

    hg: add global hgignore

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set -e
 
 HERE=$(dirname $(readlink -f $0))
 
-for f in gitconfig tmux.conf vim vimrc zshrc pythonstartup gitignore_global hgrc hgrc.d; do
+for f in gitconfig tmux.conf vim vimrc zshrc pythonstartup gitignore_global hgrc hgignore_global hgrc.d; do
 	echo ln -fsn $HERE/$f $HOME/.$f
 	ln -fsn $HERE/$f $HOME/.$f
 done

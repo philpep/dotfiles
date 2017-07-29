d97c46982e52ac2fbb07974bcbb6e5e53ff0887d
Philippe Pepiot <phil@philpep.org>
2017-07-29 23:28:00 +0200

    replace tab by space in install script

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ set -e
 HERE=$(dirname $(readlink -f $0))
 
 for f in gitconfig tmux.conf vim vimrc zshrc pythonstartup gitignore_global hgrc hgignore_global hgrc.d i3 gemrc; do
-	echo ln -fsn $HERE/$f $HOME/.$f
-	ln -fsn $HERE/$f $HOME/.$f
+    echo ln -fsn $HERE/$f $HOME/.$f
+    ln -fsn $HERE/$f $HOME/.$f
 done
 
 mkdir -p $HOME/.config/xfce4/terminal

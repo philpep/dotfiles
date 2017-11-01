d35796afd6443c48a87ac43b1a7e0874a38ed274
Philippe Pepiot <phil@philpep.org>
2017-11-01 21:13:43 +0100

    initial mutt config

--- install.sh
+++ install.sh
@@ -6,10 +6,11 @@ set -e
 
 HERE=$(dirname $(readlink -f $0))
 
-for f in gitconfig tmux.conf vim vimrc zshrc pythonstartup gitignore_global hgrc hgignore_global hgrc.d i3 gemrc; do
+for f in gitconfig tmux.conf vim vimrc zshrc pythonstartup gitignore_global hgrc hgignore_global hgrc.d i3 gemrc mutt; do
     echo ln -fsn $HERE/$f $HOME/.$f
     ln -fsn $HERE/$f $HOME/.$f
 done
 
 mkdir -p $HOME/.config/xfce4/terminal
 ln -fsn $HERE/xfce4-terminalrc $HOME/.config/xfce4/terminal/terminalrc
+mkdir -p $HOME/.cache/mutt
